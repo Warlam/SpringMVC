55caaab08984638b0c34e4292ce41d7fac974a42
warlamich <warlam2002@gmail.com>
2024-01-05 16:22:54 +0300

    1

--- src/main/java/org/springmvc/controllers/HelloController.java
+++ src/main/java/org/springmvc/controllers/HelloController.java
@@ -1,7 +1,9 @@
 package org.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,7 +12,14 @@ public class HelloController {
         return "hello/hello_world";
     }
     @GetMapping("/goodbye")
-    public String Goodbye(){
+    public String Goodbye(@RequestParam(value = "name",required = false) String name,
+                          @RequestParam(value = "surname",required = false) String surname,
+                          Model model){
+
+        //System.out.println(name);
+        //System.out.println(surname);
+        model.addAttribute("name",name);
+        model.addAttribute("surname",surname);
         return "hello/goodbye";
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,29 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
 <body>
-<h2>Hello1 World!</h2>
+<label for="firstName">Name:</label>
+<input type="text" id="firstName" placeholder="Enter name">
+
+<label for="lastName">Surname:</label>
+<input type="text" id="lastName" placeholder="Enter surname">
+
+<button onclick="redirectToGoodbye()">Login</button>
+
+<script>
+    function redirectToGoodbye() {
+        var firstName = document.getElementById("firstName").value;
+        var lastName = document.getElementById("lastName").value;
+
+        // Формируем URL с параметрами
+        var url = "http://localhost:8080/goodbye?name=" + encodeURIComponent(firstName) + "&surname=" + encodeURIComponent(lastName);
+
+        // Переходим по URL
+        window.location.href = url;
+    }
+</script>
 </body>
 </html>
